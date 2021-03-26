066f809846959deb1faae28b61d7f5b29bf08db6
Clement Denis <le.mikmac@gmail.com>
2021-03-26 15:56:38 +0000

    quests: reorganise quests

--- export.js
+++ export.js
@@ -10,6 +10,12 @@ const normalize = str => str
   .trim()
   .replace(/\s+/g, '-')
 
+const type = {
+  '06.md': 'dom',
+  '09.md': 'node',
+  '10.md': 'dom',
+}
+
 const START = 20000
 let start = START, questsData = []
 for (const name of quests) {
@@ -18,11 +24,12 @@ for (const name of quests) {
   const title = parts.shift()
   const questTitle = title.trim().slice(2).trim()
   const questName = normalize(questTitle)
-
+  const language = type[name] === 'dom' ? 'dom' : 'js'
   const exerciseProcessing = parts.map(async (md, index) => {
     const [subject, tests] = md.split('\n### Tests')
     const exerciseTitle = subject.split('\n', 1)[0]
     const exerciseName = normalize(exerciseTitle)
+    const isBonus = exerciseTitle.startsWith('🌟')
 
     // mkdir -p ./output/${exerciseName}
     // writeFile(`./output/${exerciseName}/README.md`, `## ${subject}`, 'utf8')
@@ -30,9 +37,15 @@ for (const name of quests) {
     // generate test file
     // save it
 
+    const ext = type[name] === 'node' ? 'mjs' : 'js'
     return {
+      attrs: {
+        exerciseType: isBonus ? 'optional' : undefined,
+        language,
+        displayName: exerciseTitle,
+        expectedFiles: [`${exerciseName}.${ext}`],
+      },
       name: exerciseName,
-      title: exerciseTitle,
       id: start + index + 1,
       index,
     }
@@ -45,30 +58,40 @@ for (const name of quests) {
   }
 
   exercises.length &&
-    questsData.push({ id: start, name: questName, title: questTitle, exercises })
+    questsData.push({
+      id: start,
+      index: Number(name.slice(0, -3)),
+      name: questName,
+      attrs: { language, displayName: questTitle },
+      exercises,
+    })
 
   start += MAX_QUEST_SIZE
 }
 
 const INSERT_OBJECT_VALUES = questsData.flatMap(quest => [
-  `\n  -- ${quest.title}  `,
-  `(${quest.id}, '${quest.name}', 'quest', 'online', '${JSON.stringify({ language: 'js', displayName: quest.title })}'::jsonb, '{}'::jsonb),`,
+  `\n  -- ${quest.attrs.displayName}  `,
+  `(${quest.id}, '${quest.name}', 'quest', 'online', '${JSON.stringify(quest.attrs)}'::jsonb, '{}'::jsonb),`,
   ...quest.exercises.map(exercise =>
-    `(${exercise.id}, '${exercise.name}', 'exercise', 'online', '${JSON.stringify({ language: 'js',  expectedFiles: [`${exercise.name}.js`] })}'::jsonb, '{}'::jsonb),`)
+    `(${exercise.id}, '${exercise.name}', 'exercise', 'online', '${JSON.stringify(exercise.attrs)}'::jsonb, '{}'::jsonb),`)
 ]).join('\n  ')
 
 const INSERT_RELATIONSHIP_VALUES = questsData.flatMap(quest => [
-  `\n  -- ${quest.title}  `,
+  `\n  -- ${quest.attrs.displayName}  `,
+    `(${start}, ${quest.id}, '{}'::jsonb, '${quest.name}', ${quest.index}),`,
   ...quest.exercises.map(exercise =>
     `(${quest.id}, ${exercise.id}, '{"difficulty":1}'::jsonb, '${exercise.name}', ${exercise.index+1}),`)
 ]).join('\n  ')
 
 const SQL = `
 -- cleanup
-DELETE FROM public.object WHERE id >= ${START} && id <=${start};
+DELETE FROM public.object WHERE id >= ${START} && id <= ${start};
 
 -- insert object
 INSERT INTO public.object (id, name, type, status, attrs, "childrenAttrs") VALUES
+
+  -- Discovery Piscine
+  (${start}, 'discovery-piscine', 'piscine', 'online', {"displayName":"Discovery Piscine"}'::jsonb, '{}'::jsonb),
   ${INSERT_OBJECT_VALUES.slice(0, -1)};
 
 -- insert relationships
